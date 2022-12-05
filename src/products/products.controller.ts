ebfdb0370360314cc88ca579d217594d0e7f67c7
quyendch11 <quyendch11@gmail.com>
2022-12-05 21:16:56 +0700

    upp

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,5 @@
 import { Controller } from '@nestjs/common';
-
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+@ApiTags('Product')
 @Controller('products')
 export class ProductsController {}
